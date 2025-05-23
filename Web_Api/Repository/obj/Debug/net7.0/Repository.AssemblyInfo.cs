6aebaa8e345edb4d001b218970e42751d05fa8b1
shira2005git <shira20252025@gmail.com>
2025-05-23 11:13:09 +0300

    save

--- Web_Api/Repository/obj/Debug/net7.0/Repository.AssemblyInfo.cs
+++ Web_Api/Repository/obj/Debug/net7.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b89fdd49d037d38c971270d5abfe28245a4301b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd67d66a02b930ad8e6b0f287e692639941ad64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Web_Api/Web_API/obj/Debug/net7.0/Web_API.AssemblyInfo.cs
+++ Web_Api/Web_API/obj/Debug/net7.0/Web_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b89fdd49d037d38c971270d5abfe28245a4301b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd67d66a02b930ad8e6b0f287e692639941ad64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
