4514dcb8ff907dda60b3ff5b2f5d0ea3625eb08b
SaraSARA1024 <sara248369@gmail.com>
2025-06-17 20:31:50 +0300

    Removed ignored files from the repository

---
+++ Web_Api/Web_API/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using Common.Dto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Repository.Entities;
+using Repository.Interface;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IRepository<User> repository;
+        private readonly IConfiguration config;
+        public LoginController(IRepository<User> repository, IConfiguration config)
+        {
+            this.repository = repository;
+            this.config = config;
+        }
+
+
+        // POST api/<LoginController>
+        [HttpPost("login")]
+        public IActionResult Login([FromQuery] LoginUser userLogin)
+        {
+            var user = Authenticate(userLogin);
+            if (user != null)
+            {
+                var token = Generate(user); 
+                return Ok(token);
+            }
+            return BadRequest("user does not exist");
+        }
+
+        private string Generate(User user)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var carditional = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name,user.FirstName+user.LastName),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                config["Jwt:Issuer"], config["Jwt:Audience"]
+                , claims,
+          expires: DateTime.Now.AddDays(10),
+              signingCredentials: carditional);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+
+        private User Authenticate(LoginUser loginUser)
+        {
+            var user = repository.GetAll().FirstOrDefault(x => x.Email ==loginUser.email && x.Password == loginUser.password);
+            if (user != null)
+                return user;
+            return null;
+        }
+    }
+}
---
+++ Web_Api/Web_API/Controllers/QuestionController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository.Entities;
+using Repository.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        IRepository<Question> repository;
+        public QuestionController(IRepository<Question> repository)
+        {
+            this.repository = repository;
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Question> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Question Get(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Question question)
+        {
+            repository.AddItem(question);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut]
+        public void Put([FromBody] Question question)
+        {
+            repository.UpdateItem(question);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repository.DeleteItem(id);
+        }
+    }
+}
